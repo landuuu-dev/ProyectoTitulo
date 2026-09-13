efa660c302d27a94d4470fb05232bdc48c7f6837
landuuu-dev <olachao32@gmail.com>
2026-09-13 12:47:19 -0300

    cambiando la traduccion

--- Backend/src/utils/traduccionService.js
+++ Backend/src/utils/traduccionService.js
@@ -15,10 +15,27 @@ export const traducirTexto = async (texto, idiomaOrigen, idiomaDestino) => {
     throw new Error("El texto a traducir es obligatorio.");
   }
 
+  const idiomasPermitidos = ["ES", "EN"];
+
+  if (
+    !idiomasPermitidos.includes(idiomaOrigen) ||
+    !idiomasPermitidos.includes(idiomaDestino)
+  ) {
+    throw new Error("Solo se permite traducir entre español e inglés.");
+  }
+
+  if (idiomaOrigen === idiomaDestino) {
+    throw new Error("El idioma de origen y destino deben ser diferentes.");
+  }
+
+  const idiomaOrigenDeepL = idiomaOrigen === "ES" ? "es" : "en";
+
+  const idiomaDestinoDeepL = idiomaDestino === "ES" ? "es" : "en-US";
+
   const resultado = await deeplClient.translateText(
     texto,
-    idiomaOrigen,
-    idiomaDestino,
+    idiomaOrigenDeepL,
+    idiomaDestinoDeepL,
   );
 
   return resultado.text;
